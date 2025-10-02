0352014c854c2c8002b6d05368257b252b88b8af
Erda Salihu <es7003@g.rit.edu>
2025-10-02 16:39:08 +0200

    we made some changes to the code

--- Todo.js
+++ Todo.js
@@ -1,7 +1,43 @@
-//test
+// A single class that has grown to handle too many things.
+class TodoManager {
+    constructor() {
+        this.tasks = [];
+        this.storage = new LocalStorageHandler(); 
+    }
 
+    addTask(description, type = 'simple') {
+        let task = { id: Date.now(), desc: description, completed: false };
 
+        if (type === 'urgent') {
+            task.priority = 'high';
+            task.desc = `[URGENT] ${description}`;
+        } else {
+            task.priority = 'normal';
+        }
 
-rendering () {
-    console.log("what a beautiful day to code");
+        this.tasks.push(task);
+        this.storage.save(this.tasks);
+        this.renderTasks(); 
+        return true;
+    }
+
+    renderTasks() {
+        const container = document.getElementById('task-container');
+        if (!container) return;
+
+        let html = '<ul>';
+        this.tasks.forEach(task => {
+            const style = task.completed ? 'text-decoration: line-through;' : '';
+            html += `<li style="${style}">${task.desc}</li>`;
+        });
+        html += '</ul>';
+
+        container.innerHTML = html;
+    }
+}
+
+class LocalStorageHandler {
+    save(data) {
+        console.log("SAVING to Local Storage:", data);
+    }
 }
